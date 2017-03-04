173e09ffd5a29988eedd4c485dfaf0894fdba6d7
Thomas Herzog <herzog.thomas81@gmail.com>
2017-03-04 13:11:20 +0100

    jee-7-wildfly-sso docker infrastructure

---
+++ JEE-7-SSO/Wildfly/start-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -x
+USER_ADMIN="${ADMIN_USER?admin}"
+USER_ADMIN_PASSWORD="${ADMIN_PASSWORD?admin}"
+
+${JBOSS_HOME}/bin/add-user.sh ${ADMIN_USER} ${USER_ADMIN_PASSWORD} --silent
+${JBOSS_HOME}/bin/standalone.sh --debug -b 0.0.0.0 -bmanagement 0.0.0.0
+
+exit 0
---
+++ JEE-7-SSO/Wildfly/start-test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0
+
+exit 0
