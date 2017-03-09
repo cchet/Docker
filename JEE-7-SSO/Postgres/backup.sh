d300cb94ace90c25223472d6e34144782b4cb49e
Thomas Herzog <herzog.thomas81@gmail.com>
2017-03-09 12:55:10 +0100

    Added doc, and modifications
    - Added README for jee-7-sso infrastructure
    - backup.sh drops database and recreates it on backup import
    - bind hosts in docker-compsoe for jee-7-sso

--- JEE-7-SSO/Postgres/backup.sh
+++ JEE-7-SSO/Postgres/backup.sh
@@ -5,12 +5,24 @@ if [ "$ACTION" == "EXPORT" ]
 then 
 	for entry in ${DB_NAMES//,/ } ; do 
 		DB_ARR=(${entry//=/ })
-		pg_dump -U postgres ${DB_ARR[0]} > /tmp/backups/${DB_ARR[0]}-$(date +"%Y%m%d_%H%M%S").backup.sql
+		pg_dump -U ${POSTGRES_USER} ${DB_ARR[0]} > /tmp/backups/${DB_ARR[0]}-$(date +"%Y%m%d_%H%M%S").backup.sql
 	done
 elif [ "$ACTION" == "IMPORT" ] 
 then
-	DB_ARR=(${FILE_NAME//-/ })
-	psql -U postgres -d ${DB_ARR[0]} < /tmp/backups/${FILE_NAME}
+	for entry in ${DB_NAMES//,/ } ; do 
+		RESTORE_DB_ARR=(${FILE_NAME//-/ })
+		DB_ARR=(${entry//=/ })
+		if [ "${DB_ARR[0]}" == "${RESTORE_DB_ARR[0]}" ] 
+		then
+			psql -v ON_ERROR_STOP=1 -U "${POSTGRES_USER}" <<-EOSQL
+				DROP DATABASE IF EXISTS ${DB_ARR[0]};
+				CREATE DATABASE ${DB_ARR[0]};
+				GRANT ALL PRIVILEGES ON DATABASE ${DB_ARR[0]} TO ${DB_ARR[1]};
+			EOSQL
+			psql -U ${POSTGRES_USER} -d ${DB_ARR[0]} < /tmp/backups/${FILE_NAME}
+			exit 0
+		fi
+	done	
 else
 	exit -1
 fi
--- JEE-7-SSO/Postgres/init-db.sh
+++ JEE-7-SSO/Postgres/init-db.sh
@@ -3,7 +3,7 @@
 # Creating users with passwords
 for entry in ${DB_USERS//,/ } ; do 
 	USER_ARR=(${entry//=/ })
-	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-EOSQL
+	psql -v ON_ERROR_STOP=1 -U "${POSTGRES_USER}" <<-EOSQL
         CREATE USER ${USER_ARR[0]} WITH PASSWORD '${USER_ARR[1]}';
 	EOSQL
 done
@@ -11,7 +11,7 @@ done
 # create databases and assign to user
 for entry in ${DB_NAMES//,/ } ; do 
 	DB_ARR=(${entry//=/ })
-	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-EOSQL
+	psql -v ON_ERROR_STOP=1 -U "${POSTGRES_USER}" <<-EOSQL
 		CREATE DATABASE ${DB_ARR[0]};
 		GRANT ALL PRIVILEGES ON DATABASE ${DB_ARR[0]} TO ${DB_ARR[1]};
 	EOSQL
